ae8cb3a2de3af00529442025eb228f71e5b4f417
Amila Welihinda <amilajack@gmail.com>
2017-07-17 13:19:04 -0700

    Initial example commit

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,7 +1,25 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Fiber from 'fibers';
 import styles from './Home.css';
+
+function sleep(ms) {
+  const fiber = Fiber.current;
+  setTimeout(() => {
+    fiber.run();
+  }, ms);
+  Fiber.yield();
+}
+
+Fiber(() => {
+  console.log('wait...', new Date());
+  sleep(1000);
+  console.log('ok...', new Date());
+})
+.run();
+
+console.log('back in main');
 
 export default class Home extends Component {
   render() {
